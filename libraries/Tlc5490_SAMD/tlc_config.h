542cfca0c209fae4b2da915f111f8f61a4e9f296
Joseph Urban <urbanj55@gmail.com>
2024-11-11 15:29:30 -0500

    change NUM_TLCS back to 3 as default

--- libraries/Tlc5490_SAMD/tlc_config.h
+++ libraries/Tlc5490_SAMD/tlc_config.h
@@ -61,7 +61,7 @@
     of the first TLC to the SIN (TLC pin 26) of the next.  The rest of the pins
     are attached normally.
     \note Each TLC needs it's own IREF resistor */
-#define NUM_TLCS    1
+#define NUM_TLCS    3
 
 /** Determines how data should be transfered to the TLCs.  Bit-banging can use
     any two i/o pins.
